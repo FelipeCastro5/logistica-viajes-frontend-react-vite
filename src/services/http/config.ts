d0abd5eeaeb7510b7ec0970c00a2d24bf86b3b6b
Daniel Castro <danielfelipecastrolizarazo@gmail.com>
2026-01-08 18:25:03 -0500

    deploy

--- src/services/http/config.ts
+++ src/services/http/config.ts
@@ -1,8 +1,8 @@
 const ENV = import.meta.env.VITE_ENV || "dev"
 
 const API_BASE_URLS: Record<string, string> = {
-  local: import.meta.env.VITE_API_LOCAL || "https://logistica-viajes-backend-nest-remesa.onrender.com/",
-  dev: import.meta.env.VITE_API_DEV || "",
+  local: import.meta.env.VITE_API_LOCAL || "",
+  dev: import.meta.env.VITE_API_DEV || "https://logistica-viajes-backend-nest-remesa.onrender.com/",
   prod: import.meta.env.VITE_API_PROD || ""
 }
 
